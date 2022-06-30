d5a4a1ba44602ee93514609582a7262ba5fdffb4
Pavel <dzygman@glincom.com>
2022-06-30 15:22:33 +0300

    finischfirstlesson

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-console.log("Hello world")
+console.log('Hello world')
 
 let Tc = 32;
 let Tf = (9 / 5) * Tc + 32;
 
-alert(Tf);
+console.log(Tf);
+
+let admin;
+
+let name;
+
+name = 'Великий';
+
+admin = name;
+
+console.log(admin);
+
+console.log(1000 + "108");
+
