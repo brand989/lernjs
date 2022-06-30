65ba1389dc6b9f8fd9696692fe34acc0dc1d5ee3
Pavel <dzygman@glincom.com>
2022-06-30 18:55:04 +0300

    finish secondLesson

--- main.js
+++ main.js
@@ -1,19 +1,103 @@
-console.log('Hello world')
+/* 3 task
+let a = Number(prompt("введите число а"));
+let b = Number(prompt("введите число b"));
 
-let Tc = 32;
-let Tf = (9 / 5) * Tc + 32;
+if(a>=0 && b>=0){
+    console.log(a-b)
+} else if(a<0 && b<0){
+    console.log(a*b)
+} else {
+    console.log(a+b)
+}
 
-console.log(Tf);
+*/
 
-let admin;
+/* 4 task
+let a = Number(prompt("введите число а"));
 
-let name;
+switch(a){
+    case 1:
+        console.log("1")     
+    case 2:
+        console.log("2")
+    case 3:
+        console.log("3")     
+    case 4:
+        console.log("4")
+    case 5:
+        console.log("5")     
+    case 6:
+        console.log("6")
+    case 7:
+        console.log("7")     
+    case 8:
+        console.log("8")
+    case 9:
+        console.log("9")     
+    case 10:
+        console.log("10")
+        break;
+}
 
-name = 'Великий';
+*/
 
-admin = name;
+/*
 
-console.log(admin);
+ //5 task
+function sum(a,b){
+    return a+b
+}
 
-console.log(1000 + "108");
+function sub(a,b){
+    return a-b
+}
 
+function mult(a,b){
+    return a*b
+}
+
+function div(a,b){
+    return a/b
+}
+
+
+//6 task
+function mathOperation(arg1, arg2, operation){
+    switch(operation){
+        case "+":
+            return sum(arg1,arg2)
+            break
+        case "-":
+            return sub(arg1,arg2)
+            break
+        case "*":
+            return mult(arg1,arg2)
+            break
+        case "/":
+            return div(arg1,arg2)
+            break
+    }
+}
+
+let a = Number(prompt("введите число а"));
+let b = Number(prompt("введите число b"));
+let operation = (prompt("введите оперецию"));
+
+console.log(mathOperation(a,b,operation))
+
+*/
+
+
+// 7 task
+function power(val, pow){
+    if(pow == 0){
+        return 1
+    }
+    pow--
+    return val*power(val,pow)
+}
+
+let a = Number(prompt("введите число"));
+let b = Number(prompt("введите степень"));
+
+console.log(power(a,b));
