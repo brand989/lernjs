ae1f97f87a67511a4c226b9506cee5a4bde9607a
Pavel <dzygman@glincom.com>
2022-07-12 20:35:32 +0300

    add button in basket

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 class Product {
 
+
     constructor(name, price) {
         this.name = name
         this.price = price
+        this.count = 1
     }
+
+
 
     render() {
         let main = document.querySelector('main')
@@ -14,10 +18,21 @@ class Product {
         item.innerHTML = `${this.name} стоит ${this.price}`
         list.appendChild(item)
         let button = new Button('Купить')
-        button.render(item, this.name, this.price )
+        button.render(item, '+', this)
 
     }
 
+
+    inc() {
+        this.count++
+    }
+
+
+    dec() {
+        if (this.count > 0) {
+            this.count--
+        }
+    }
 }
 
 class List {
@@ -28,7 +43,7 @@ class List {
         this.render()
     }
 
-    render(){
+    render() {
         this._products.forEach(element => element.render());
     }
 
@@ -38,20 +53,64 @@ class List {
 class Basket {
     _products = []
 
-    constructor(...products) {
-        this._products.push(...products)
+    constructor() {
+
+        if (Basket._instance) {
+            return Basket._instance
+        }
+
+        Basket._instance = this
     }
 
     countBasketPrice() {
         return this._products.reduce(function (sum, elem) {
-            return sum + elem.price;
+            return sum + elem.price * elem.count;
         }, 0);
     }
 
     addProducts(product) {
-        this._products.push(product)
+
+        let j = 0
+        for (let i = 0; i < this._products.length; i++) {
+
+            if (product.name == this._products[i].name) {
+                this._products[i].inc()
+            } else {
+                j++
+            }
+        }
+
+        if (j == this._products.length) {
+            this._products.push(product)
+
+        }
+
         this.render()
+
     }
+
+    delProducts(product) {
+
+        for (let i = 0; i < this._products.length; i++) {
+
+            if (product.name == this._products[i].name) {
+
+                if (this._products[i].count == 1) {
+                    this._products.splice([i], 1)
+                    console.log(this._products)
+                } else {
+                    this._products[i].dec()
+                }
+            }
+        }
+
+
+
+        this.render()
+
+    }
+
+
 
     render() {
         let header = document.querySelector('header')
@@ -62,8 +121,13 @@ class Basket {
 
         for (let i = 0; i < this._products.length; i++) {
             let item = document.createElement('div')
-            item.innerHTML = `${this._products[i].name} по цене ${this._products[i].price}`
+            item.innerHTML = `${this._products[i].name} по цене ${this._products[i].price} в количестве  ${this._products[i].count}`
             list.appendChild(item)
+            let buttonInc = new Button('+')
+            buttonInc.render(item, '+', this._products[i])
+
+            let buttonDec = new Button('-')
+            buttonDec.render(item, '-', this._products[i])
         }
 
         let totalPrice = document.createElement('div')
@@ -83,9 +147,8 @@ class Button {
     }
 
     onClick(fn, item) {
-        console.log(fn, item)
-        fn(item)
 
+        fn(item)
     }
 
     getHTML() {
@@ -94,13 +157,29 @@ class Button {
         return btn
     }
 
-    render(blockToRender, name, price) {
+    render(blockToRender, or, obj) {
+
         const btn = this.getHTML()
         blockToRender.appendChild(btn)
 
+
         btn.addEventListener('click', () => {
-            this.onClick(basketUser.addProducts.bind(basketUser), {name, price})
+            const basketUser = new Basket()
+
+            if (or == "+") {
+                let fn = basketUser.addProducts.bind(basketUser)
+                this.onClick(fn, obj)
+            } else if (or == "-") {
+                let fn = basketUser.delProducts.bind(basketUser)
+                this.onClick(fn, obj)
+            }
+
+            
+
+           
         })
+
+
     }
 }
 
@@ -111,7 +190,7 @@ let laptop = new Product('laptop', 4500);
 let mobile = new Product('mobile', 5500);
 let tablet = new Product('tablet', 2100);
 
-const basketUser = new Basket(tablet);
+const basketUser = new Basket();
 
 
 const newList = new List(mobile, tablet, laptop)
@@ -120,4 +199,3 @@ basketUser.addProducts(laptop);
 
 
 basketUser.render()
-
