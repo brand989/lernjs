4ee9831bdb1ca0d2f569e85de46de443c967fb7e
Pavel <dzygman@glincom.com>
2022-07-21 16:52:23 +0300

    add Vue

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <header class="header">
+     <div v-bind:class="[logo]"> My shop </div>
+      <div v-bind:class="{ cart: isActive }"><!-- <Cart /> -->Корзина</div>
+    </header>
+    <main>
+      <h1>Товары</h1>
+
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() { return {
+    logo:'logo',
+    isActive: true
+  }
+  },
+  methods: {},
+};
+</script>
+
+<style module>
+    .header {
+    min-height: 100px;
+    background-color: blueviolet;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    }
+    .logo {
+    color: white;
+    padding: 30px;
+    font-size: 40px;
+    }
+    .cart {
+    color: white;
+    font-weight: bold;
+    padding: 15px;
+    }
+    main h1 {
+    padding: 30px;
+    }
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,48 @@
-const path = require('path');
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-    entry: './src/main.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'shop.js',
     },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm.js'
+        }
 
+    },
     module: {
         rules: [{
-            test: /\.css$/,
-            use: [
-                {
-                    loader: 'style-loader'
-                },
-                {
-                    loader: 'css-loader'
-                }
-               
-            ]
-        },
-        {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader'            
-            }
-          }
-    
-    ]
+                test: /\.vue$/,
+                use: [
+                    {loader: 'vue-loader'},
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: 'vue-style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: [
+                    { loader: 'babel-loader'},
+                ]
+            },
+
+        ]
     },
+
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+
 };
